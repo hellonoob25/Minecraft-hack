1cac9928b4fe6905a87ae9d624c79dfb3017a9d7
情雲工作室-魚生 <amy12182002@gmail.com>
2025-08-27 20:06:57 +0800

    Update 圖形介面V1(優先注入).java

--- 圖形介面V1(優先注入).java
+++ 圖形介面V1(優先注入).java
@@ -24,8 +24,8 @@ public class MyGuiScreen extends Screen {
         // 功能 kill aura按鈕，只有在分類 展開時才顯示
         if (showCategoryA) {
             this.addRenderableWidget(new Button(this.width / 2 - 50, this.height / 2 + 10, 100, 20, Component.literal("kill aura++"), button -> {
-                // 點擊功能 C 執行 killaura.java 裡的邏輯
-                H.execute();
+                // 點擊功能 執行 killaura 裡的邏輯
+                kill_aura_pro.execute();
             }));
         }
     }
